6b10afdeb30e4585c26d11122f4e480e621e0919
Marcin <m.zaorski12@gmail.com>
2026-02-16 00:35:06 +0100

    Adding Vite dev sever config file

---
+++ frontend/vite.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  server: {
+    host: true,
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://web:8000",
+        changeOrigin: true, 
+        secure: false,
+      },
+      "/media": {
+        target: "http://web:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+      // optionally static or other endpoints:
+      "/static": {
+        target: "http://web:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },  
+});
+//https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
